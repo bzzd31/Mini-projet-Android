bc612c0420e9b718e6b8436c08bc8282189b0896
Lucas B <lucas.bled12@gmail.com>
2016-01-15 11:52:18 +0100

    Ajout du GPS, affichage longitude latitude, affichage statut GPS, demande activation GPS si désactivité au démarrage de l'appli

--- app/src/main/java/com/m2dl/mini_projet/mini_projet_android/MainActivity.java
+++ app/src/main/java/com/m2dl/mini_projet/mini_projet_android/MainActivity.java
@@ -1,22 +1,49 @@
 package com.m2dl.mini_projet.mini_projet_android;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager locationManager = null;
+    private TextView textViewGPS = null, textViewLatLong = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewGPS = (TextView)findViewById(R.id.textViewGPS);
+        textViewLatLong = (TextView)findViewById(R.id.textViewLatLong);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        } else {
+            textViewGPS.setText("GPS ON");
+            textViewLatLong.setText("Recherche en cours...");
+        }
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +53,48 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Votre GPS semble désactivé, voulez-vous l'activer ? (Le GPS est essentiel pour le bon fonctionnement de l'application)")
+                .setCancelable(false)
+                .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        String str = "Latitude: "+location.getLatitude()+"\nLongitude: "+location.getLongitude();
+        textViewLatLong.setText(str);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        textViewGPS.setText("GPS OFF");
+        textViewLatLong.setText("En attente du GPS");
+        //buildAlertMessageNoGps();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        textViewGPS.setText("GPS ON");
+        textViewLatLong.setText("Recherche en cours...");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
