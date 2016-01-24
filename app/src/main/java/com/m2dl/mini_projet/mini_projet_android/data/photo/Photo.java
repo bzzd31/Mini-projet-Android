13e01575a6db9f2421cac98cbf9a8aae53d8f90f
Tetote <theo.vaucher@gmail.com>
2016-01-24 01:09:13 +0100

    Add Date and getter in Photo

--- app/src/main/java/com/m2dl/mini_projet/mini_projet_android/data/photo/Photo.java
+++ app/src/main/java/com/m2dl/mini_projet/mini_projet_android/data/photo/Photo.java
@@ -3,6 +3,7 @@ package com.m2dl.mini_projet.mini_projet_android.data.photo;
 import com.m2dl.mini_projet.mini_projet_android.data.tag.Tag;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Photo {
@@ -10,17 +11,19 @@ public class Photo {
     private double coordLat;
     private double coordLong;
     private String author;
+    private Date date;
     private List<Tag> tags;
     private String url;
 
     public Photo(String author, double coordLat, double coordLong) {
-        this(author, coordLat, coordLong, null);
+        this(author, coordLat, coordLong, null, null);
     }
 
-    public Photo(String author, double coordLat, double coordLong, String url) {
+    public Photo(String author, double coordLat, double coordLong, Date date, String url) {
         this.author = author;
         this.coordLat = coordLat;
         this.coordLong = coordLong;
+        this.date = date;
         this.url = url;
         this.tags = new ArrayList<>();
     }
@@ -29,12 +32,39 @@ public class Photo {
         this.tags.add(tag);
     }
 
+    public double getCoordLat() {
+        return coordLat;
+    }
+
+    public double getCoordLong() {
+        return coordLong;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
     @Override
     public String toString() {
-        return "["+coordLat +
-                ", " + coordLong +
-                ", " + author +
-                ", " + tags +
-                ']';
+        return "Photo{" +
+                "coordLat=" + coordLat +
+                ", coordLong=" + coordLong +
+                ", author='" + author + '\'' +
+                ", date=" + date +
+                ", tags=" + tags +
+                ", url='" + url + '\'' +
+                '}';
     }
 }
--- app/src/main/java/com/m2dl/mini_projet/mini_projet_android/provider/IPhotoProvider.java
+++ app/src/main/java/com/m2dl/mini_projet/mini_projet_android/provider/IPhotoProvider.java
@@ -5,11 +5,12 @@ import android.graphics.Bitmap;
 import com.m2dl.mini_projet.mini_projet_android.data.photo.Photo;
 import com.m2dl.mini_projet.mini_projet_android.data.tag.Tag;
 
+import java.util.Date;
 import java.util.List;
 
 public interface IPhotoProvider {
 
     List<Photo> getPhotos();
 
-    Photo post(Bitmap photo, String author, double coordLat, double coordLong, List<Tag> tags);
+    Photo post(Bitmap photo, String author, Date date, double coordLat, double coordLong, List<Tag> tags);
 }
