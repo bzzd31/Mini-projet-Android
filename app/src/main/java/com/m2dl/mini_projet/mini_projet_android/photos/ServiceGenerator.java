73282e83cc91ca21b553b173c277ca84c58ee369
kelto <charles.fallourd@gmail.com>
2016-01-26 21:06:01 +0100

    set date format for gson with same format on server

--- app/src/main/java/com/m2dl/mini_projet/mini_projet_android/photos/ServiceGenerator.java
+++ app/src/main/java/com/m2dl/mini_projet/mini_projet_android/photos/ServiceGenerator.java
@@ -1,10 +1,13 @@
 package com.m2dl.mini_projet.mini_projet_android.photos;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.OkHttpClient;
 
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
+import retrofit.converter.GsonConverter;
 
 /**
  * Created by kelto on 21/01/16.
@@ -13,12 +16,17 @@ public class ServiceGenerator {
 
     //check if data should be removed
 
-
+    private static Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            .create();
     private static RestAdapter.Builder builder = new RestAdapter.Builder()
             .setEndpoint(Constants.API_URL)
-            .setClient(new OkClient(new OkHttpClient()));
+            .setClient(new OkClient(new OkHttpClient()))
+            .setConverter(new GsonConverter(gson));
+
 
     public static <S> S createService(Class<S> serviceClass) {
+
         RestAdapter adapter = builder.build();
         return adapter.create(serviceClass);
     }
