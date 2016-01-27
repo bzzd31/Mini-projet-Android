e58faddbc0f9f642c581052ab92eb6e86f268cc6
kelto <charles.fallourd@gmail.com>
2016-01-27 14:09:57 +0100

    remove useless functions and class

--- app/src/main/java/com/m2dl/mini_projet/mini_projet_android/photos/SimpleImageTag.java
+++ app/src/main/java/com/m2dl/mini_projet/mini_projet_android/photos/SimpleImageTag.java
@@ -1,7 +1,6 @@
 package com.m2dl.mini_projet.mini_projet_android.photos;
 
 import com.m2dl.mini_projet.mini_projet_android.photos.model.Photo;
-import com.m2dl.mini_projet.mini_projet_android.photos.model.PhotoList;
 
 import java.util.List;
 
@@ -9,19 +8,11 @@ import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
-import retrofit.http.Query;
 
 /**
  * Created by kelto on 24/01/16.
  */
 public interface SimpleImageTag {
-    @GET("/images")
-    PhotoList getPhotos();
-
-    @POST("/images")
-    void postPhoto(
-            @Body Photo photo
-    );
 
     @GET("/images")
     void getAsyncPhotos(Callback<List<Photo>> callback);
